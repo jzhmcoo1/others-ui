a9a0882c5b28aeb0985da79a36cbe9d22f98ae91
yanyunwu <2480721346@qq.com>
2024-01-31 17:14:50 +0800

    feat: select组件完善

--- packages/others-ui-docs/components/select.md
+++ packages/others-ui-docs/components/select.md
@@ -3,10 +3,14 @@
 <br>
 
 <div>
-  <ot-select :options="options"/>
+  <div>{{val}}</div>
+  <ot-select :options="options" @change="val = $event.detail"/>
 </div>
 
 <script setup>
+  import { ref } from 'vue'
+
+  const val = ref(0)
 
   const options = [
     {
@@ -16,6 +20,18 @@
     {
       label: '第二个',
       value: "2"
+    },
+    {
+      label: '第三个',
+      value: "3"
+    },
+    {
+      label: '第四个',
+      value: "4"
+    },
+    {
+      label: '第五个',
+      value: "5"
     }
   ]
 </script>
--- packages/others-ui/src/components/select/select.ts
+++ packages/others-ui/src/components/select/select.ts
@@ -1,8 +1,9 @@
-import { css, html, unsafeCSS } from '@others-ui/common'
+import { css, html, unsafeCSS, styleMap, PropertyValueMap, ref, createRef, Ref } from '@others-ui/common'
 import { property, state } from '@others-ui/common'
-import styles from './styles/select.scss'
 import { BaseElement } from '@others-ui/common'
 import { ifDefined } from '@others-ui/common'
+import styles from './styles/select.scss'
+import { watch } from '../../utils/watch'
 
 export interface SelectOptions {
   label?: string,
@@ -31,22 +32,63 @@ export class Select extends BaseElement implements SelectProps {
   @state()
   private label?: string
 
+  @state()
+  private styles_css_variable
+
+  rootRef?: Ref<HTMLDivElement> = createRef<HTMLDivElement>()
+
+  constructor() {
+    super()
+    this.styles_css_variable = {
+      '--others-ui-scope-select-list-height': '0'
+    }
+  }
+
+  connectedCallback() {
+    super.connectedCallback()
+    this.styles_css_variable['--others-ui-scope-select-list-height']= `${this.options.length * 30}px`
+  }
+
   onItemClick(option: SelectOptions) {
     this.label = option.label ?? option.value
     this._value = option.value
     this.emit('change', this._value)
+    this.styles_css_variable = {
+      ...this.styles_css_variable,
+      '--others-ui-scope-select-list-height': '0'
+    }
+
+    setTimeout(() => {
+      this.styles_css_variable['--others-ui-scope-select-list-height'] = `${this.options.length * 30}px`
+      console.log('this.rootRef?.value', this.rootRef?.value)
+      if (this.rootRef?.value) {
+        this.rootRef.value.style.setProperty('--others-ui-scope-select-list-height', `${this.options.length * 30}px`)
+      }
+
+    }, 300)
   }
 
+  protected willUpdate(state: PropertyValueMap<SelectOptions>) {
+    watch(state, {
+      value: (val) => this._value = val
+    })
+  }
 
   render() {
     return html`
-    <div class="select">
-      <div class="select-active">
+    <div
+      class="select"
+      style=${styleMap(this.styles_css_variable)}
+      ${ref(this.rootRef)}
+    >
+      <div class="select-active-label">
         ${ifDefined(this.label)}
       </div>
-      <ul class="select-list">
-        ${this.options.map((option) => renderSelectItem.call(this, option))}
-      </ul>
+      <div class="select-list">
+        <ul class="select-list-box">
+          ${this.options.map((option) => renderSelectItem.call(this, option))}
+        </ul>
+      </div>
     </div>
     `
   }
